bf22350c864cc7023ba1956ea146dad93e70d3f7
손량 <loop.infinitely@gmail.com>
2018-09-10 23:44:42 +0900

    added non-working example

--- pyqt-code-editor/pyqt-code-editor.py
+++ pyqt-code-editor/pyqt-code-editor.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python
-import os
 import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
 
+
+class Window(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self, None)
+        self.init_ui()
+
+    def init_ui(self):
+        self.text = QTextEdit(self)
+        self.text.setTabStopWidth(12)
+        self.setCentralWidget(self.text)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
 
 if __name__ == '__main__':
-	print 'It works!'
+    main()
